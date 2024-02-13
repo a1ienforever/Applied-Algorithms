ac75e9391976852e9bb2a0fcbf557128df5e5e11
alienforever <artyom.zadvornyj@yandex.ru>
2024-02-13 23:26:32 +0300

    2

--- lb1/test.py
+++ lb1/test.py
@@ -1,6 +1,6 @@
 import csv
 import random
-from collections import defaultdict
+from random import randint
 
 
 class Node:
@@ -15,76 +15,96 @@ class Node:
     def print_nodes(self):
         nodes = ''
         for node in self.nodes:
-            nodes += str(node.data) + ','
+            nodes += str(node.data) + ' '
         return nodes
 
 
+class Cluster:
+    def __init__(self):
+        self.nodes = set()
+
+    def size_graphs(self, num_vertices):
+        nums = []
+        num = 0
+        groups = {}
+        new_arr = []
+        while num_vertices > num:
+            if num_vertices > 10:
+                count_vert = randint(0, int(num_vertices / 10))
+            else:
+                count_vert = randint(0, int(num_vertices))
+            num_vertices -= count_vert
+            nums.append(count_vert)
+
+        for num in nums:
+            if num in groups:
+                groups[num] += num
+            else:
+                groups[num] = num
+
+        for num, total in groups.items():
+            new_arr.append(total)
+        return new_arr
+
+    def create_disconnected_graph(self, num_vertices, avg_connectivity):
+        size_graphs = self.size_graphs(num_vertices)
+
+        for i in range(len(size_graphs)):
+            self.nodes.add(Graph().create_connected_graph(size_graphs[i], avg_connectivity))
+        return self
+
+    def print(self):
+        for graph in self.nodes:
+            for node in graph.list_node:
+                print(node.__str__())
+            print('-----------------------')
+
 class Graph:
     def __init__(self):
-        self.list_node = []
+        self.list_node = list()
         self.sum_conn = 0
 
-    def create_connected_graph(self, num_vertices, avg_connectivity):
+    def create_connected_graph(self, num_vertices=None, avg_connectivity=None, num_start=1):
         u = int(avg_connectivity - (avg_connectivity / 2))
         v = int(avg_connectivity + (avg_connectivity / 2))
-        # visited = set()
-        for i in range(1, num_vertices + 1):
+        for i in range(num_start, num_vertices + 1):
             self.list_node.append(Node(i))
 
         for node in self.list_node:
             rand_conn_num = random.randint(u, v)
             self.sum_conn += rand_conn_num
-            # nodes = set(self.list_node) - visited
             for _ in range(rand_conn_num):
-                # node1 = random.choice(tuple(nodes))
                 node1 = random.choice(self.list_node)
                 if node1 not in node.nodes and node1 != node:
                     node.nodes.add(node1)
                     node1.nodes.add(node)
-                    # visited.add(node1)
 
         return self
 
     def write_graph_to_csv(self, filename, graph):
         with open(filename, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
+            writer = csv.writer(csvfile, delimiter=' ')
 
             # Write vertices
-            writer.writerow(['Vertices'])
-            writer.writerow([node.data for node in graph.list_node])
-
-            # Write adjacency list
-            writer.writerow(['Adjacency List'])
+            writer.writerow(f'Vertices;Abjacency_List')
             for node in graph.list_node:
-                writer.writerow([' '.join(str(n.data) for n in node.nodes)])
+                writer.writerow([f'{node.data};', ';'.join(str(n.data) for n in node.nodes)])
+                print([f'{node.data};', ';'.join(str(n.data) for n in node.nodes)])
 
+    def print(self):
+        for node in self.list_node:
+            print(node.__str__())
 
-
-
-
-    # def average_connectivity(self):
-    #     adjacency_matrix = defaultdict(int)
-    #
-    #     for node in self.list_node:
-    #         for connected_node in node.nodes:
-    #             adjacency_matrix[(node.data, connected_node.data)] = 1
-    #             adjacency_matrix[(connected_node.data, node.data)] = 1
-    #
-    #     total_edges = sum(adjacency_matrix.values())
-    #     num_vertices = len(self.list_node)
-    #     avg = total_edges / num_vertices
-    #     return avg
 
 
 def main():
-    graph = Graph()
-    graph1 = graph.create_connected_graph(10, 5)
-    # print(graph1.average_connectivity())
-    # print(graph1.is_connected())
-    graph.write_graph_to_csv('graph.csv', graph1)
-    for node in graph1.list_node:
-        print(node.__str__())
+    graph_conn = Graph().create_connected_graph(50, 5)
+    graph_conn.print()
+    graph_conn.write_graph_to_csv('graph.csv', graph_conn)
 
+
+    # graph_disconn = Cluster().create_disconnected_graph(50, 5)
+    # graph_disconn.print()
 
 if __name__ == "__main__":
     main()
