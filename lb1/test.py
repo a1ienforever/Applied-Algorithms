ece5a8f716e17915a5712ab42104d76bf9efe5fc
alienforever <artyom.zadvornyj@yandex.ru>
2024-02-14 16:45:11 +0300

    2

--- lb1/test.py
+++ lb1/test.py
@@ -15,7 +15,7 @@ class Node:
     def print_nodes(self):
         nodes = ''
         for node in self.nodes:
-            nodes += str(node.data) + ' '
+            nodes += str(node.data) + ','
         return nodes
 
 
@@ -24,7 +24,7 @@ class Cluster:
         self.nodes = set()
 
     def size_graphs(self, num_vertices):
-        nums = []
+        graphs = []
         num = 0
         groups = {}
         new_arr = []
@@ -34,9 +34,9 @@ class Cluster:
             else:
                 count_vert = randint(0, int(num_vertices))
             num_vertices -= count_vert
-            nums.append(count_vert)
+            graphs.append(count_vert)
 
-        for num in nums:
+        for num in graphs:
             if num in groups:
                 groups[num] += num
             else:
@@ -58,16 +58,19 @@ class Cluster:
             for node in graph.list_node:
                 print(node.__str__())
             print('-----------------------')
+        print(len(self.nodes))
+
 
 class Graph:
     def __init__(self):
         self.list_node = list()
         self.sum_conn = 0
 
-    def create_connected_graph(self, num_vertices=None, avg_connectivity=None, num_start=1):
+    def create_connected_graph(self, num_vertices=None, avg_connectivity=None):
         u = int(avg_connectivity - (avg_connectivity / 2))
         v = int(avg_connectivity + (avg_connectivity / 2))
-        for i in range(num_start, num_vertices + 1):
+        edge = 0
+        for i in range(1, num_vertices + 1):
             self.list_node.append(Node(i))
 
         for node in self.list_node:
@@ -78,33 +81,40 @@ class Graph:
                 if node1 not in node.nodes and node1 != node:
                     node.nodes.add(node1)
                     node1.nodes.add(node)
-
+                    edge += 1
+        # print(2 * edge / num_vertices)
         return self
 
     def write_graph_to_csv(self, filename, graph):
         with open(filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=' ')
-
-            # Write vertices
-            writer.writerow(f'Vertices;Abjacency_List')
             for node in graph.list_node:
-                writer.writerow([f'{node.data};', ';'.join(str(n.data) for n in node.nodes)])
-                print([f'{node.data};', ';'.join(str(n.data) for n in node.nodes)])
+                writer.writerow([f'{node.data},', ','.join(str(n.data) for n in node.nodes)])
 
     def print(self):
         for node in self.list_node:
             print(node.__str__())
 
 
-
 def main():
-    graph_conn = Graph().create_connected_graph(50, 5)
-    graph_conn.print()
-    graph_conn.write_graph_to_csv('graph.csv', graph_conn)
+    with open("C:\\Users\\artyo\\PycharmProjects\\Applied Algorithms\\lb1\\Configuration", mode='r') as config:
+        config_reader = config.read()
+        arr_param = config_reader.split(' ')
+        num_vertices = int(arr_param[0])
+        is_connected = arr_param[1] == 'y'
+        avg_connectivity = int(arr_param[2])
 
+        # Generate the graph
 
-    # graph_disconn = Cluster().create_disconnected_graph(50, 5)
-    # graph_disconn.print()
+        if is_connected:
+            graph = Graph().create_connected_graph(num_vertices, avg_connectivity)
+            graph.print()
+        else:
+            graph = Cluster().create_disconnected_graph(num_vertices, avg_connectivity)
+            graph.print()
+
+    graph.write_graph_to_csv('graph.csv', graph)
+
 
 if __name__ == "__main__":
     main()
